386a3ee390b0d24cc66e38925cd31f5de17f14b7
HuseynSadatkhanovHolberton <1261holbertonstudents.com>
2025-12-07 14:20:07 +0000

    Task 6 The shortest distance between two points is a straight line

--- more_functions_nested_loops/main.c
+++ more_functions_nested_loops/main.c
@@ -1,8 +1,16 @@
 #include "main.h"
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	more_numbers();
-	return 0;
+	print_line(0);
+	print_line(2);
+	print_line(10);
+	print_line(-4);
+	return (0);
 }
 
