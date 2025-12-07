99c0680459a80f2aa9b07a2712a9f79ec9940dad
HuseynSadatkhanovHolberton <1261holbertonstudents.com>
2025-12-07 12:59:13 +0000

    Task 10 a+b

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -1,8 +1,19 @@
 #include "main.h"
+#include <stdio.h> /* local testing only */
 
 int main(void)
 {
-    times_table();
+    int r;
+
+    r = add(1, 2);
+    printf("%d\n", r);
+
+    r = add(98, 1024);
+    printf("%d\n", r);
+
+    r = add(-5, 7);
+    printf("%d\n", r);
+
     return (0);
 }
 
