3722c974b14f85ced1538f6762b67a217e6df179
HuseynSadatkhanovHolberton <1261holbertonstudents.com>
2025-12-07 14:05:32 +0000

    Task 3 The numbers speak for themselves

--- more_functions_nested_loops/main.c
+++ more_functions_nested_loops/main.c
@@ -1,13 +1,8 @@
 #include "main.h"
-#include <stdio.h> /* ONLY for testing */
 
 int main(void)
 {
-	printf("%d\n", mul(5, 3));     /* 15 */
-	printf("%d\n", mul(-2, 4));    /* -8 */
-	printf("%d\n", mul(-7, -6));   /* 42 */
-	printf("%d\n", mul(0, 12345)); /* 0 */
-
+	print_numbers();
 	return (0);
 }
 
