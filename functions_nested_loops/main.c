a7215085e4fddf36c4103e7ef49f25638694cdb1
HuseynSadatkhanovHolberton <1261holbertonstudents.com>
2025-12-07 12:52:07 +0000

    Task 8 I'm federal agent Jack Bauer, and today is the longest day of my life

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -3,17 +3,7 @@
 
 int main(void)
 {
-	int r;
-
-	r = print_last_digit(98);
-	printf(" -> %d\n", r);
-
-	r = print_last_digit(0);
-	printf(" -> %d\n", r);
-
-	r = print_last_digit(-1024);
-	printf(" -> %d\n", r);
-	
-	return (0);
+	jack_bauer();
+       	return (0);
 }
 
