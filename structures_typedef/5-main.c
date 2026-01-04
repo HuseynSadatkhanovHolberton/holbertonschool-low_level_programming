e1e0d0b63483ff8510edfd13ebe10114a6158982
HuseynSadatkhanovHolberton <12671@holbertonstudents.com>
2026-01-04 20:23:48 +0000

    Task 5 How many legs does a dog have if you call his tail a leg? Four. Saying that a tail is a leg doesn't make it a leg

--- structures_typedef/5-main.c
+++ structures_typedef/5-main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include "dog.h"
+#include <stdlib.h>
+#include "dog.h"  /* <--- Must include this to know free_dog */
 
 int main(void)
 {
@@ -7,8 +8,9 @@ int main(void)
 
     my_dog = new_dog("Poppy", 3.5, "Bob");
     printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
-    free_dog(my_dog);
 
-    return 0;
+    free_dog(my_dog);  /* now compiler knows about free_dog */
+
+    return (0);
 }
 
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,16 +2,16 @@
 #define DOG_H
 
 /**
- * struct dog - structure describing a dog
+ * struct dog - describes a dog
  * @name: dog's name
  * @age: dog's age
  * @owner: dog's owner
  */
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+    char *name;
+    float age;
+    char *owner;
 };
 
 /* typedef for struct dog */
@@ -21,6 +21,7 @@ typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d); /* THIS LINE MUST EXIST */
 
 #endif /* DOG_H */
 
