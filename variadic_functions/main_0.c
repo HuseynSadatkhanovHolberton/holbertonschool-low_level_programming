3caeaca6c826f60a4ce0c175129bc2a93cc90295
HuseynSadatkhanovHolberton <12671@holbertnstudents.com>
2026-01-11 15:28:57 +0000

    main_0.c

---
+++ variadic_functions/main_0.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+
+/**
+ * main - test print_numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_numbers("; ", 5, 30, 938, -123024, 43402, 0);
+	return (0);
+}
+
