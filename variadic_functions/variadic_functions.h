28a4e2496dd7ebce05d8edb12c31e9c01cf9fc40
HuseynSadatkhanovHolberton <12671@holbertnstudents.com>
2026-01-11 15:40:49 +0000

    variadic_functions.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -12,6 +12,9 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 /* Task 2 */
 void print_strings(const char *separator, const unsigned int n, ...);
 
+/* Task 3 */
+void print_all(const char * const format, ...);
+
 /* Provided by checker */
 int _putchar(char c);
 
