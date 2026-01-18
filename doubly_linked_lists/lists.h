88a7cc0f66b318d536951db1434766808a3f1978
HuseynSadatkhanovHolberton <12671@holbertonstudents.com>
2026-01-18 14:58:21 +0000

    lists.h

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -23,6 +23,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n);       /* Task 2 */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);   /* Task 3 */
 void free_dlistint(dlistint_t *head);                  /* Task 4 */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index); /* Task 5 */
+int sum_dlistint(dlistint_t *head);                    /* Task 6 */
 
 #endif /* LISTS_H */
 
