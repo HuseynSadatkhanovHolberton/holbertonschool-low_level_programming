bcd120d78bd473768c8de9a3e7fc668bfd96d39c
HuseynSadatkhanovHolberton <12671@holbertonstudents.com>
2026-01-18 14:39:39 +0000

    Task 3 Add node at the end

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -16,18 +16,18 @@ list_t *add_node_end(list_t **head, const char *str)
 	unsigned int len = 0;
 
 	if (str == NULL)
-		return NULL;
+		return (NULL);
 
 	/* Allocate memory for new node */
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
-		return NULL;
+		return (NULL);
 
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
 		free(new_node);
-		return NULL;
+		return (NULL);
 	}
 
 	/* Compute string length */
@@ -40,16 +40,11 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (*head == NULL)
 	{
 		*head = new_node;
-		return new_node;
+		return (new_node);
 	}
 
 	/* Traverse to last node */
-	current = *head;          /* <-- THIS LINE MUST BE COMPLETE */
+	current = *head;
 	while (current->next != NULL)
-		current = current->next;
-
-	current->next = new_node;
-
-	return new_node;
-}
+		current = cur
 
