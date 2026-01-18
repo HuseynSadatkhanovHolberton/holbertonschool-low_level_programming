e5c9767a391a2281452d67c2702afedf58bc7678
HuseynSadatkhanovHolberton <12671@holbertonstudents.com>
2026-01-18 14:21:52 +0000

    lists.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -16,8 +16,9 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-/* Prototypes */
+/* Function prototypes */
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 
 #endif /* LISTS_H */
 
