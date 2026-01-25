8e9992ee5a152ef8c14518c8bc211b36bfe9925e
HuseynSadatkhanovHolberton <12671@holbertonstudents.com>
2026-01-25 13:20:38 +0000

    Task 3 cp

---
+++ file_io/3-cp.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * main - copies the content of a file to another file
+ * @argc: number of arguments
+ * @argv: argument vector
+ *
+ * Return: 0 on success, exits with specific codes on error
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to;
+	ssize_t r_bytes, w_bytes;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+
