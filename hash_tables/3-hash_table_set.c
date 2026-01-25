5acfcf1578c5d83aa310f317488d0e7ea80e0b93
HuseynSadatkhanovHolberton <12671@holbertonstudents.com>
2026-01-25 13:49:42 +0000

    Task 3 >>>ht['betty'] = 'cool'

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -3,59 +3,59 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - adds or updates an element in the hash table
- * @ht: hash table
- * @key: key (cannot be empty)
- * @value: value (must be duplicated)
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table pointer
+ * @key: key string
+ * @value: value string
  *
- * Return: 1 if success, 0 otherwise
+ * Return: 1 on success, 0 on failure
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int index;
-    hash_node_t *new_node, *current;
+	unsigned long int index;
+	hash_node_t *new_node, *tmp;
 
-    if (!ht || !key || *key == '\0')
-        return (0);
+	if (!ht || !key || *key == '\0' || !value)
+		return (0);
 
-    /* Compute index */
-    index = key_index((const unsigned char *)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[index];
 
-    /* Check if key already exists, update value */
-    current = ht->array[index];
-    while (current)
-    {
-        if (strcmp(current->key, key) == 0)
-        {
-            /* Replace old value */
-            free(current->value);
-            current->value = strdup(value ? value : "");
-            if (!current->value)
-                return (0);
-            return (1);
-        }
-        current = current->next;
-    }
+	while (tmp)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);
+			if (!tmp->value)
+				return (0);
+			return (1);
+		}
+		tmp = tmp->next;
+	}
 
-    /* Key not found, create new node */
-    new_node = malloc(sizeof(hash_node_t));
-    if (!new_node)
-        return (0);
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
 
-    new_node->key = strdup(key);
-    new_node->value = strdup(value ? value : "");
-    if (!new_node->key || !new_node->value)
-    {
-        free(new_node->key);
-        free(new_node->value);
-        free(new_node);
-        return (0);
-    }
+	new_node->key = strdup(key);
+	if (!new_node->key)
+	{
+		free(new_node);
+		return (0);
+	}
 
-    /* Insert at beginning of list for collisions */
-    new_node->next = ht->array[index];
-    ht->array[index] = new_node;
+	new_node->value = strdup(value);
+	if (!new_node->value)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
 
-    return (1);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
 }
 
