91f3f6b1abf88057be26425fd1922056266a1792
HuseynSadatkhanovHolberton <12671@holbertonstudents.com>
2026-01-25 13:19:53 +0000

    main.h

--- file_io/main.h
+++ file_io/main.h
@@ -9,6 +9,9 @@ ssize_t read_textfile(const char *filename, size_t letters);
 /* Task 1 */
 int create_file(const char *filename, char *text_content);
 
+/* Task 2 */
+int append_text_to_file(const char *filename, char *text_content);
+
 /* Provided helper */
 int _putchar(char c);
 
