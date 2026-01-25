3f026e66fca1f3208964d943f226505279152fb9
HuseynSadatkhanovHolberton <12671@holbertonstudents.com>
2026-01-25 13:38:14 +0000

    hash_tables.h

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -3,7 +3,7 @@
 
 #include <stdlib.h>
 
-/* Node structure */
+/* Task 0: Hash node structure */
 typedef struct hash_node_s
 {
     char *key;
@@ -11,18 +11,21 @@ typedef struct hash_node_s
     struct hash_node_s *next;
 } hash_node_t;
 
-/* Hash table structure */
+/* Task 0: Hash table structure */
 typedef struct hash_table_s
 {
     unsigned long int size;
     hash_node_t **array;
 } hash_table_t;
 
-/* Task 0 */
+/* Task 0: Create a hash table */
 hash_table_t *hash_table_create(unsigned long int size);
 
-/* Task 1 */
+/* Task 1: djb2 hash function */
 unsigned long int hash_djb2(const unsigned char *str);
+
+/* Task 2: Get index of a key */
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
 
 #endif /* HASH_TABLES_H */
 
