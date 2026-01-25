540f6f6baa26a5b48e375532b1ff058b7c70e2e1
HuseynSadatkhanovHolberton <12671@holbertonstudents.com>
2026-01-25 13:34:00 +0000

    main_0.c

--- hash_tables/main_0.c
+++ hash_tables/main_0.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "hash_tables.h" /* Must include for hash_djb2 prototype */
+#include "hash_tables.h" /* MUST include for hash_djb2 prototype */
 
-/**
- * main - check the code
- *
- * Return: Always EXIT_SUCCESS
- */
 int main(void)
 {
-	char *s;
+    char *s;
 
-	s = "cisfun";
-	printf("%lu\n", hash_djb2((unsigned char *)s));
+    s = "cisfun";
+    printf("%lu\n", hash_djb2((unsigned char *)s));
 
-	s = "Don't forget to tweet today";
-	printf("%lu\n", hash_djb2((unsigned char *)s));
+    s = "Don't forget to tweet today";
+    printf("%lu\n", hash_djb2((unsigned char *)s));
 
-	s = "98";
-	printf("%lu\n", hash_djb2((unsigned char *)s));
+    s = "98";
+    printf("%lu\n", hash_djb2((unsigned char *)s));
 
-	return (EXIT_SUCCESS);
+    return (EXIT_SUCCESS);
 }
 
