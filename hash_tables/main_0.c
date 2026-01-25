765bef26c4f0137482f91bc0a4647c5b72312608
HuseynSadatkhanovHolberton <12671@holbertonstudents.com>
2026-01-25 13:38:44 +0000

    main_0.c

--- hash_tables/main_0.c
+++ hash_tables/main_0.c
@@ -1,26 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "hash_tables.h" /* needed for hash_djb2 & key_index */
+#include "hash_tables.h" /* needed for all prototypes */
 
+/**
+ * main - test hash_table_create, hash_djb2, key_index
+ *
+ * Return: Always EXIT_SUCCESS
+ */
 int main(void)
 {
-	char *s;
-	unsigned long int hash_table_array_size;
+    hash_table_t *ht;
+    char *s;
+    unsigned long int array_size;
 
-	hash_table_array_size = 1024;
+    /* Task 0: Create a hash table of size 1024 */
+    ht = hash_table_create(1024);
+    printf("Hash table pointer: %p\n", (void *)ht);
 
-	s = "cisfun";
-	printf("%lu\n", hash_djb2((unsigned char *)s));
-	printf("%lu\n", key_index((unsigned char *)s, hash_table_array_size));
+    /* Task 1 & 2: djb2 and key_index */
+    array_size = 1024;
+    s = "cisfun";
+    printf("Hash for '%s': %lu\n", s, hash_djb2((unsigned char *)s));
+    printf("Index for '%s': %lu\n", s, key_index((unsigned char *)s, array_size));
 
-	s = "Don't forget to tweet today";
-	printf("%lu\n", hash_djb2((unsigned char *)s));
-	printf("%lu\n", key_index((unsigned char *)s, hash_table_array_size));
+    s = "Don't forget to tweet today";
+    printf("Hash for '%s': %lu\n", s, hash_djb2((unsigned char *)s));
+    printf("Index for '%s': %lu\n", s, key_index((unsigned char *)s, array_size));
 
-	s = "98";
-	printf("%lu\n", hash_djb2((unsigned char *)s));
-	printf("%lu\n", key_index((unsigned char *)s, hash_table_array_size));
+    s = "98";
+    printf("Hash for '%s': %lu\n", s, hash_djb2((unsigned char *)s));
+    printf("Index for '%s': %lu\n", s, key_index((unsigned char *)s, array_size));
 
-	return (EXIT_SUCCESS);
+    return (EXIT_SUCCESS);
 }
 
